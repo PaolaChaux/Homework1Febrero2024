5abeceefa66cce3f588e8739a991e871528eece2
Paola Chaux Campo <chauxcampo@gmail.com>
2024-02-16 18:07:35 -0500

    version2 - Pao

--- 1.R
+++ 1.R
@@ -2,8 +2,20 @@
 #Universidad Autónoma de Occidente
 #HomeWork 1
 ########################################################################################
-install.packages("openxlsx")
 
+install.packages("readr")
 
 #Cargar librerias
-library(openxlsx)
+library(readr)
+
+#Fijar el directorio de trabajo
+setwd("P:/Estadistica y probabilidad 2/Homework1 febrero")
+
+# Listar los archivos en el directorio de trabajo
+dir()
+
+#Cargar los datos
+DF <- read_csv("dataset_HW1_insurance.csv")
+
+# Visualizar las primera filas del conjunto de datos
+head(DF)
